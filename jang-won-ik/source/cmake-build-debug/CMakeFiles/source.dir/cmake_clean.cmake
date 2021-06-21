24675ada2d414ba256cd0a8b11a1b6676a9a0663
dhslrl321 <dhslrl321@gmail.com>
2021-06-22 00:07:50 +0900

    commit

---
+++ jang-won-ik/source/cmake-build-debug/CMakeFiles/source.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/source.dir/main.c.o"
+  "source"
+  "source.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/source.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
