6ea1369dcee7af4865fc3abc69d8aca45432eaeb
dhslrl321 <dhslrl321@gmail.com>
2021-07-02 17:27:35 +0900

    commit

--- jang-won-ik/source/keyword_3/pointer1.c
+++ jang-won-ik/source/keyword_3/pointer1.c
@@ -6,30 +6,11 @@ int main() {
 
     int *pointer = &number; // number 변수의 주소값 저장
 
-    printf("--number 의 주소값 출력-- \n");
-    printf("%#010x \n", pointer);
-    printf("%#010x \n", &number);
-    printf("%#010x \n", &*pointer);
-    printf("\n\n");
+    assert(&number == pointer);
+    assert(number == *pointer);
+    assert(&number != &pointer);
 
-    printf("--number 에 할당된 값 출력-- \n");
-    printf("%d \n", number);
-    printf("%d \n", *pointer);
-    printf("\n\n");
+    printf("테스트 성공");
 
-    printf("--pointer 의 주소값 출력-- \n");
-    printf("%#010x \n", &pointer);
-    printf("\n\n");
-
-    // 참조 연산자의 사용
-    int number2 = 100;
-
-    int *pointer2 = &number2;
-
-    assert(pointer2 == &number2);
-
-    *pointer2 = 200;
-    assert(number2 == 200);
-    assert(*pointer2 == 200);
     return 0;
 }
